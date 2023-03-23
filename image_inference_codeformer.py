8c8720905f1d238c2d913a906ac612843d210ad7
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-03-23 09:41:10 +0800

    add test code

--- image_inference_codeformer.py
+++ image_inference_codeformer.py
@@ -2,6 +2,7 @@ import torch
 from PIL import Image
 import numpy as np
 from RealESRGAN import RealESRGAN
+import cv2
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -9,8 +10,9 @@ model = RealESRGAN(device, scale=4)
 model.load_weights('weights/RealESRGAN_x4.pth', download=True)
 
 path_to_image = 'inputs/xx.png'
-image = Image.open(path_to_image).convert('RGB')
-
+# image = Image.open(path_to_image).convert('RGB')
+# image = np.array(image)
+image = cv2.cvtColor(cv2.imread(path_to_image), cv2.COLOR_BGR2RGB)
 sr_image = model.predict(image)
 sr_image = Image.fromarray(sr_image)
 sr_image.save('results/xx.png')
